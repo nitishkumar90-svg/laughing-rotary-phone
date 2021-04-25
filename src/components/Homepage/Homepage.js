19b54ff88e71070bb1d5421066bb0d6d35f76adb
Nitish Kumar <70220240+nitishkumar90-svg@users.noreply.github.com>
2021-04-24 20:48:14 -0400

    [SM] Updated Changes

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Footer from '../Shared/Footer'
+import Header from '../Shared/Header'
 
 const Homepage = () => {
   return (
+    <>
+      <Header />
       <h1>Hello Flone!!</h1>
+      <Footer />
+    </>
   )
 }
 
