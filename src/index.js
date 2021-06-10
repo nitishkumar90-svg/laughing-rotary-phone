8477cc427adce7230ddd5a17245b57dec538a97f
Nitish Kumar <70220240+nitishkumar90-svg@users.noreply.github.com>
2021-06-10 15:48:38 -0400

    Your message new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { HashRouter } from 'react-router-dom';
-import './css/index.css'
+import './css/index.css'   
 import App from './components/App'
 import { MainContext } from './Context/mainContext';
 
